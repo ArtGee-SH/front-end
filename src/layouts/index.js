a20393a21ba7cc5dab2093a526c5fd510549b2e8
my <yang.yang@kucoinc.com>
2020-05-23 11:43:58 +0800

    mod: 调整布局

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 
 import {Web3InjectProvider} from '@/context/web3inject';
 import {PolkadotProvider} from '@/context/polkadot';
+import { AccountsProvider } from '@/context/accounts';
 import Head from '@/components/Head';
 
 
@@ -11,12 +12,14 @@ function BasicLayout(props) {
   return (
     <Web3InjectProvider>
       <PolkadotProvider>
+        <AccountsProvider>
         <div className={styles.app_wrapper}>
           <Head />
           <div className={styles.app_body}>
             {props.children}
           </div>
         </div>
+        </AccountsProvider>
       </PolkadotProvider>
     </Web3InjectProvider>
   );
